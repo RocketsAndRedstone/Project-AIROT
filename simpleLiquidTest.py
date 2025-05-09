6e5b2a3c13c38a459378908139c2da93b4f7cac4
RocketsAndRedstome <dwlethgo@gmail.com>
2025-05-08 21:20:52 -0400

    added fist pass at fitst stage auto pilot controls

--- simpleLiquidTest.py
+++ simpleLiquidTest.py
@@ -5,11 +5,21 @@ vessel.control.sas = True
 
 vessel.control.activate_next_stage()
 vessel.control.throttle = 1
+
+#sets up controll for autopilot
 vessel.auto_pilot.engage()
+vessel.auto_pilot.roll_error(2)
+vessel.auto_pilot.heading_error(5)
+vessel.auto_pilot.pitch_error(10)
+vessel.auto_pilot.pitch_pid_gains(0.25)
+vessel.auto_pilot.yaw_pid_gains(0.25)
+
 time.sleep(1)
 vessel.control.activate_next_stage()
-time.sleep(10)
-while  vessel.flight().heading != 200:
-    vessel.control.yaw = 0.25
+time.sleep(5)
+vessel.auto_pilot.target_roll(90)
+time.sleep(5)
+vessel.auto_pilot.target_pitch_and_heading(45, 180)
+
 
 #vessel.control.pitch = 1
