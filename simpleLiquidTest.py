a124cf83b7d3ce3cda3fa7fb1b6c469544bbec81
dwlethgo@gmail.com <dwlethgo@gmail.com>
2025-05-10 18:09:19 -0400

    added roll program

--- simpleLiquidTest.py
+++ simpleLiquidTest.py
@@ -3,10 +3,16 @@ conn = krpc.connect(name="Launch Liquid 1")
 vessel = conn.space_center.active_vessel
 vessel.control.sas = True
 
+#engine ignition and pad seperation
 vessel.control.activate_next_stage()
 vessel.control.throttle = 1
 time.sleep(1)
 vessel.control.activate_next_stage()
 
 
-#vessel.control.pitch = 1
+#posible roll program
+
+vessel.auto_pilot.engage()
+vessel.auto_pilot.target_roll(90)
+vessel.auto_pilot.wait()
+print("roll program complete")
