afcba81bd77017810775f04742189997d16ff1b5
dwlethgo@gmail.com <dwlethgo@gmail.com>
2025-05-10 20:35:22 -0400

    added PID loop for roll program with adjustable gains

--- simpleLiquidTest.py
+++ simpleLiquidTest.py
@@ -12,7 +12,26 @@ vessel.control.activate_next_stage()
 
 #posible roll program
 
-vessel.auto_pilot.engage()
-vessel.auto_pilot.target_roll(90)
-vessel.auto_pilot.wait()
-print("roll program complete")
+time.sleep(5)
+
+prevError = 0
+intergral = 0
+targetRoll = -90
+dt = 0.2
+proportinalGain = 0.2
+intergralGain = 0.1
+derivGain = 0.1
+
+while not (targetRoll - 0.5 < vessel.flight().roll < targetRoll + 0.5):
+    error =   targetRoll - vessel.flight().roll
+    proportinal = error
+    intergral = (intergral + error) * dt
+    derivative = (error - prevError) / dt
+
+    vessel.control.roll = (proportinalGain * proportinal) + (intergralGain * intergral) + (derivGain * derivative)
+
+    prevError = error
+    time.sleep(dt)
+
+
+print("roll program complete at" , vessel.flight().roll)
