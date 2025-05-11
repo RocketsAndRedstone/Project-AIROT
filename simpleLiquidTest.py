affcffe0bad2d4f2015ba4abacaed507dea87859
dwlethgo@gmail.com <dwlethgo@gmail.com>
2025-05-11 13:06:55 -0400

    added abort thread, rearanged program to use methods

--- simpleLiquidTest.py
+++ simpleLiquidTest.py
@@ -1,37 +1,48 @@
-import krpc , time
-conn = krpc.connect(name="Launch Liquid 1")
-vessel = conn.space_center.active_vessel
-vessel.control.sas = True
+import krpc , time , threading
 
-#engine ignition and pad seperation
-vessel.control.activate_next_stage()
-vessel.control.throttle = 1
-time.sleep(1)
-vessel.control.activate_next_stage()
+def main():
+    conn = krpc.connect(name="Launch Liquid 1")
+    vessel = conn.space_center.active_vessel
+    vessel.control.sas = True
 
+    abortThread = threading.Thread(target=checkAbort, args=(vessel))
 
-#posible roll program
+    #engine ignition and pad seperation
+    vessel.control.activate_next_stage()
+    vessel.control.throttle = 1
+    time.sleep(1)
+    vessel.control.activate_next_stage()
+    time.sleep(5)
+    rollProgram(vessel)
 
-time.sleep(5)
+    abortThread.start()
 
-prevError = 0
-intergral = 0
-targetRoll = -90
-dt = 0.2
-proportinalGain = 0.2
-intergralGain = 0.1
-derivGain = 0.1
+def rollProgram(vessel):
+    prevError = 0
+    intergral = 0
+    targetRoll = -90
+    dt = 0.5
+    proportinalGain = 0.2
+    intergralGain = 0.2
+    derivGain = 0.1
 
-while not (targetRoll - 0.5 < vessel.flight().roll < targetRoll + 0.5):
-    error =   targetRoll - vessel.flight().roll
-    proportinal = error
-    intergral = (intergral + error) * dt
-    derivative = (error - prevError) / dt
+    while not (targetRoll - 0.5 < vessel.flight().roll < targetRoll + 0.5):
+        error =   targetRoll - vessel.flight().roll
+        proportinal = error
+        intergral = (intergral + error) * dt
+        derivative = (error - prevError) / dt
 
-    vessel.control.roll = (proportinalGain * proportinal) + (intergralGain * intergral) + (derivGain * derivative)
+        vessel.control.roll = (proportinalGain * proportinal) + (intergralGain * intergral) + (derivGain * derivative)
 
-    prevError = error
-    time.sleep(dt)
+        prevError = error
+        time.sleep(dt)
 
+    print("roll program complete at" , vessel.flight().roll)
 
-print("roll program complete at" , vessel.flight().roll)
+def checkAbort(vessel):
+    while(vessel.flight().surfaceAltitude > 20 & vessel.flight().surfaceAltitude < 10000):
+        if(vessel.flight().pitch < 60):
+            vessel.control.abort = True
+
+if __name__ == "__main__":
+    main()
