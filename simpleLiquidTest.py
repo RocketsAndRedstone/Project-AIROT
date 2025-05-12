4b2d1c67b2b092f449a9a186b59e31f9dcc4f69a
dwlethgo@gmail.com <dwlethgo@gmail.com>
2025-05-11 20:17:28 -0400

    adjusted roll program gain values and various other parameters, large ossolations occour with target rolls near +- 180

--- simpleLiquidTest.py
+++ simpleLiquidTest.py
@@ -5,28 +5,32 @@ def main():
     vessel = conn.space_center.active_vessel
     vessel.control.sas = True
 
-    abortThread = threading.Thread(target=checkAbort, args=(vessel))
+    abortThread = threading.Thread(target=checkAbort, args=(vessel,))
+    rollThread = threading.Thread(target=rollProgram, args=(vessel,))
 
     #engine ignition and pad seperation
     vessel.control.activate_next_stage()
     vessel.control.throttle = 1
     time.sleep(1)
     vessel.control.activate_next_stage()
-    time.sleep(5)
-    rollProgram(vessel)
 
+    time.sleep(5)    
+    rollThread.start()
     abortThread.start()
+
+    rollThread.join()
+    abortThread.join()
 
 def rollProgram(vessel):
     prevError = 0
     intergral = 0
     targetRoll = -90
-    dt = 0.5
-    proportinalGain = 0.2
-    intergralGain = 0.2
-    derivGain = 0.1
+    dt = 0.25
+    proportinalGain = 0.05
+    intergralGain = 0.025
+    derivGain = 0.025
 
-    while not (targetRoll - 0.5 < vessel.flight().roll < targetRoll + 0.5):
+    while not (targetRoll - 0.15 < vessel.flight().roll < targetRoll + 0.15):
         error =   targetRoll - vessel.flight().roll
         proportinal = error
         intergral = (intergral + error) * dt
@@ -36,13 +40,16 @@ def rollProgram(vessel):
 
         prevError = error
         time.sleep(dt)
+    vessel.control.roll = 0
 
     print("roll program complete at" , vessel.flight().roll)
 
 def checkAbort(vessel):
-    while(vessel.flight().surfaceAltitude > 20 & vessel.flight().surfaceAltitude < 10000):
-        if(vessel.flight().pitch < 60):
+    while((vessel.flight().surface_altitude > 20) and (vessel.flight().surface_altitude < 5000)):
+        if(vessel.flight().pitch < 35):
             vessel.control.abort = True
+            print("Aborted")
+            break
 
 if __name__ == "__main__":
     main()
