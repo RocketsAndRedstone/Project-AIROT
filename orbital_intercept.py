1155d1cc3232967e0453a8d4ec0fd8f754e3637d
dwlethgo@gmail.com <dwlethgo@gmail.com>
2025-12-18 21:45:05 -0500

    Added function to calculate the relative angle between the two crafts

--- orbital_intercept.py
+++ orbital_intercept.py
@@ -1,5 +1,6 @@
 #importing needed libraries
 import krpc
+import math
 from time import sleep
 from threading import Thread , Event
 from CircleQueue import CircleQueue
@@ -17,8 +18,28 @@ def main():
             interceptor = crafts[i]
         elif (crafts[i].name == "Agena Space Station"):         
             targetCraft = crafts[i]
-        else:
-            continue
+
+    print(calculateRelativeAngle(interceptor , targetCraft))
+
+def calculateRelativeAngle(interceptor, targetCraft) -> float:
+    #kerbin radius for degree to meter calculation
+    kerbinRadius = 600000
+
+    #getting each vessel's longitude
+    targetLongitude = targetCraft.flight().longitude
+    interceptorLongitude = interceptor.flight().longitude
+
+    #converting the longitude to a standard distance
+    targetLongitude = math.radians(targetLongitude) * kerbinRadius
+    interceptorLongitude = math.radians(interceptorLongitude) * kerbinRadius
+
+    #angle calculations
+    targetAngle = math.degrees(math.atan(targetCraft.flight().surface_altitude / targetLongitude))
+    interceptAngle = math.degrees(math.atan(interceptor.flight().surface_altitude / interceptorLongitude))
+
+    relativeAngle = interceptAngle + targetAngle 
+
+    return relativeAngle
 
 if (__name__ == "__main__"):
     main()
